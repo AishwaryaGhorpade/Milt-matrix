e6a8f8440d687d53ec1581545402ffff39ec048c
Aishwarya Ghorpade <aishwaryaghorpade4@gmail.com>
2025-03-18 17:19:07 +0530

    refactor: migrate frontend to TypeScript and update project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+function App() {
+
+  return (
+    <>
+      <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+    </>
+  )
+}
+
+export default App
