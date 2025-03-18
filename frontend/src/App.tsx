923820b275590419b7b8f907a1c0a9c0f220d142
Aishwarya Ghorpade <aishwaryaghorpade4@gmail.com>
2025-03-18 20:18:46 +0530

    refactor: remove unused backend files and update frontend imports and navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,11 @@
 import './App.css'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+import Navigation from './components/Navigation'
 
 function App() {
 
   return (
     <>
-      <NavigationMenu>
-  <NavigationMenuList>
-    <NavigationMenuItem>
-      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-      <NavigationMenuContent>
-        <NavigationMenuLink>Link</NavigationMenuLink>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
-  </NavigationMenuList>
-</NavigationMenu>
+      <Navigation/>
     </>
   )
 }
